310ab48661ea5517c67bf96977a41f64b988e49a
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 15:41:10 -0600

    creacion

---
+++ Lab2 Digital 2/main.c
@@ -0,0 +1,20 @@
+/*
+ * Lab2 Digital 2.c
+ *
+ * Created: 22/01/2026 03:36:18 p. m.
+ * Author : David Carranza
+ */ 
+
+// Pantalla LCD Y ACD
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
