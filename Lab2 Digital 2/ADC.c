70f96090b8d12053f0d112a9b45f2f91efd263b4
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 15:43:11 -0600

    Librerias ADC

---
+++ Lab2 Digital 2/ADC.c
@@ -0,0 +1,6 @@
+/*
+ * ADC.c
+ *
+ * Created: 22/01/2026 03:42:54 p. m.
+ *  Author: Admin
+ */ 
---
+++ Lab2 Digital 2/ADC.h
@@ -0,0 +1,16 @@
+/*
+ * ADC.h
+ *
+ * Created: 22/01/2026 03:42:41 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+
+
+
+#endif /* ADC_H_ */
