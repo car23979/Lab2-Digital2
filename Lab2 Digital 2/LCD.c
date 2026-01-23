38707b0257af9e67bae42889288ee9004963a614
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 19:19:44 -0600

    creación librerias LCD

---
+++ Lab2 Digital 2/LCD.c
@@ -0,0 +1,6 @@
+/*
+ * LCD.c
+ *
+ * Created: 22/01/2026 07:05:44 p. m.
+ *  Author: Admin
+ */ 
---
+++ Lab2 Digital 2/LCD.h
@@ -0,0 +1,16 @@
+/*
+ * LCD.h
+ *
+ * Created: 22/01/2026 07:05:59 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+
+
+
+#endif /* LCD_H_ */
