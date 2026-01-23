722245d8831db470f55eba0e982f49e307c6a313
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 19:27:41 -0600

    Comandos
    comando para incremento

--- Lab2 Digital 2/LCD.h
+++ Lab2 Digital 2/LCD.h
@@ -12,9 +12,11 @@
 #include <stdint.h>
 
 // Comandos LCD
-#define LCD_clear		0x01
-#define LCD_
-
+#define LCD_CLEAR		0x01	// Comando para limpiar pantalla
+#define LCD_8BIT		0x38	// Comando para tener la LCD en 8Bits
+#define LCD_HOME		0x02	// Comando para inicio
+#define LCD_ON			0x0C	// Comando para encender pantalla
+#define LCD_ENTRY		0x06	// Comando para Incremento
 
 
 #endif /* LCD_H_ */
