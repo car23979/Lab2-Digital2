2e030436cc4ec3089bbd8cc3371c9a31a74ddd7c
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 19:20:45 -0600

    Codigo clase

--- Lab2 Digital 2/LCD.c
+++ Lab2 Digital 2/LCD.c
@@ -4,3 +4,57 @@
  * Created: 22/01/2026 07:05:44 p. m.
  *  Author: Admin
  */ 
+
+
+ #include "LCD.h"
+
+ // Función para inicializar LCD en modo para 4 bits
+ void initLCD4bits(void)
+ DDRC |= (1)
+
+
+
+
+ /////////////////////////////////////////
+ // Function Set
+
+
+ // Clear Display
+
+
+ // Función para enviar un comando
+ void LCD_CMD(char a) {
+	 // RS = 0; // => RS = 1
+	 
+ }
+
+ // Función para colocar en el puerto un valor
+ void LCD_Port(char a) {
+	 if (a & 1)
+	 // D4 = 1;
+	 PORTB |
+	 if (a & 2)
+	 // D5 = 1
+ }
+
+ // fUNCION PARA ENVIAR UN CARACTER
+ void LCD_WRITE_CHAR(char c) {
+	 char Cbajo, Calto;
+	 Cbajo = c & 0x0F;
+	 Calto = c & ;
+	 
+ }
+
+ // Función para enviar una cadena
+ void
+
+ // Desplazamiento hacia la izquierda
+
+ // Establecer el cursor
+ void LCD_Set_Cursor(char c, char f) {
+	 char temp, talto, tbajo;
+	 if (f == 1) {
+		 temp = 0x80 + 1c;
+		 
+	 }
+ }
