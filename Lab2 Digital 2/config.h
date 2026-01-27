f8e51872ecdd194f0de79f64e0a50bceb0ef7c15
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-27 12:51:05 -0600

    Configuración
    Se creo una libreria de configuración para definir los 16MHZ

---
+++ Lab2 Digital 2/config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ * Created: 27/01/2026 12:49:25 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define F_CPU 16000000L		// 16Mhz
+
+
+
+#endif /* CONFIG_H_ */
