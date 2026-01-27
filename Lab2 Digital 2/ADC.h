c790a9a49b81f2d5e6be8d03c50af9d28694c8d5
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-27 12:54:45 -0600

    Float
    Se elimino la función float porque ocupa mucha memoria

--- Lab2 Digital 2/ADC.h
+++ Lab2 Digital 2/ADC.h
@@ -6,13 +6,12 @@
  */ 
 
 
-#ifndef ADC_H
-#define ADC_H
+#ifndef ADC_H_
+#define ADC_H_
 
 #include <stdint.h>
 
 void ADC_Init(void);
 uint16_t ADC_Read(uint8_t channel);
-float ADC_ReadVoltage(uint8_t channel);
 
 #endif
